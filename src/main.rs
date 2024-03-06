145397e8fe17f7c5ebab7e61acb1f11dfed23bdd
Mathew Sherry <mjsherry248@gmail.com>
2024-03-06 04:35:22 -0500

    Add simple game flow for first minigame (touch the yellow)

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,48 @@
-use macroquad::ui::{hash, root_ui, widgets};
+use macroquad::{color::*, input::TouchPhase, input::*, shapes::*, window::*};
 
-use macroquad::prelude::*;
+use rand::{thread_rng, Rng};
 
-#[macroquad::main("Events")]
+#[macroquad::main("Game")]
 async fn main() {
+    let mut input_x: f32 = -999.0;
+    const NUM_LOCATIONS: i32 = 3;
+    let mut rng = thread_rng();
+    let mut current_location: i32 = rng.gen_range(1..=NUM_LOCATIONS);
+
     loop {
-        clear_background(BLACK);
-        draw_rectangle(0.0, 0.0, screen_width() / 3.0, screen_height(), DARKGRAY);
+        clear_background(DARKGRAY);
         draw_rectangle(
-            screen_width() / 3.0,
+            screen_width() * (current_location as f32 - 1.0) / NUM_LOCATIONS as f32,
             0.0,
-            screen_width() / 3.0,
+            screen_width() / NUM_LOCATIONS as f32,
             screen_height(),
             YELLOW,
         );
-        draw_rectangle(
-            screen_width() * 2.0 / 3.0,
-            0.0,
-            screen_width() / 3.0,
-            screen_height(),
-            DARKGRAY,
-        );
-        let (mouse_x, _) = mouse_position();
-        if is_mouse_button_down(MouseButton::Left)
-            && mouse_x > screen_width() / 3.0
-            && mouse_x < screen_width() * 2.0 / 3.0
-        {
-            draw_text(
-                "SUCCESS!!!",
-                screen_width() / 2.0,
-                screen_height() / 2.0,
-                30.0,
-                BLACK,
-            );
+
+        for touch in touches() {
+            if touch.phase == TouchPhase::Started {
+                input_x = touch.position.x;
+            }
         }
+
+        let (mouse_x, _) = mouse_position();
+        if is_mouse_button_pressed(MouseButton::Left) {
+            input_x = mouse_x;
+        }
+
+        if input_x > screen_width() * (current_location as f32 - 1.0) / NUM_LOCATIONS as f32
+            && input_x < screen_width() * current_location as f32 / NUM_LOCATIONS as f32
+        {
+            let prev_location = current_location;
+            while current_location == prev_location {
+                current_location = rng.gen_range(1..=NUM_LOCATIONS);
+            }
+            // TODO: Reward dog via serial commands
+            // TODO: Keep track of dog's skill level in persistent storage and reward based on skill
+            // TODO: Add option to switch between dog profiles and to manually adjust skill level in-game
+        }
+
+        input_x = -999.0;
         next_frame().await;
     }
 }
